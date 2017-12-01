ca1c2d629aab065742f7f0403e9589d8651f02a3
Lokarde <hugo.lecourt@gmail.com>
2017-12-01 12:53:46 +0100

    lastest

---
+++ InfoscordBot.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json')
+const token = config.token;
+
+var adminProfile = JSON.parse('{}');
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('userUpdate',
+(oldUser, newUser) => {
+        adminProfile[newUser.id] = {};
+        adminProfile[newUser.id]['username'] = newUser.username;
+        adminProfile[newUser.id]['avatarURL'] = newUser.avatarURL;
+        fs.writeFile("/var/www/infoscord.lokarde.fr/adminProfile.json", JSON.stringify(adminProfile), function(err) {
+                if(err) {
+                        return console.log(err);
+                }
+        });
+});
+
+client.on('guildMemberUpdate',
+(oldMember, newMember) => {
+        adminProfile[newMember.user.id] = {};
+        adminProfile[newMember.user.id]['username'] = newMember.user.username;
+        adminProfile[newMember.user.id]['avatarURL'] = newMember.user.avatarURL;
+        fs.writeFile("/var/www/infoscord.lokarde.fr/adminProfile.json", JSON.stringify(adminProfile), function(err) {
+                if(err) {
+                        return console.log(err);
+                }
+        });
+});
+
+client.login(token);
