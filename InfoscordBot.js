8d8ff785c3ab06b12e64023d32edb2d77d158f9c
hugo <Lokarde>
2017-12-01 22:39:35 +0100

    correction

--- InfoscordBot.js
+++ InfoscordBot.js
@@ -65,7 +65,7 @@ function destruct(channel, msg) {
       db["word"][msg_t[w1]]["channel"] = {}
       db["word"][msg_t[w1]]["name"] = msg_t[w1];
     }
-    for (var w2 in db) {
+    for (var w2 in db["word"]) {
       if (comp(msg_t[w1], db["word"][w2]["name"]) < config.matchn) {
         console.log(msg_t[w1], db["word"][w2]["name"]);
         words.push(db["word"][w2]["name"]);
