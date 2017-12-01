dc7cec524b7d1df69d3d3876161d628ebb02e876
hugo <Lokarde>
2017-12-01 19:02:54 +0100

    letters + word comp

--- InfoscordBot.js
+++ InfoscordBot.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const config = require('./config.json')
+const client = new Discord.Client();
+
+const db = JSON.parse(fs.readFileSync("db.json"));
 const token = config.token;
 
 var adminProfile = JSON.parse(fs.readFileSync(config.webroot + "/adminProfile.json"));
@@ -11,29 +13,66 @@ client.on('ready', () => {
 });
 
 client.on('userUpdate',
-(oldUser, newUser) => {
-        adminProfile[newUser.id] = {};
-        adminProfile[newUser.id]['username'] = newUser.username;
-        adminProfile[newUser.id]['avatarURL'] = newUser.avatarURL;
-        fs.writeFile(config.webroot + "/adminProfile.json", JSON.stringify(adminProfile), function(err) {
-                if(err) {
-                        return console.log(err);
-                }
-        });
-});
+  (oldUser, newUser) => {
+    adminProfile[newUser.id] = {};
+    adminProfile[newUser.id]['username'] = newUser.username;
+    adminProfile[newUser.id]['avatarURL'] = newUser.avatarURL;
+    fs.writeFile(config.webroot + "/adminProfile.json", JSON.stringify(adminProfile), function(err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  });
 
 client.on('guildMemberUpdate',
-(oldMember, newMember) => {
-        adminProfile[newMember.user.id] = {};
-        adminProfile[newMember.user.id]['username'] = newMember.user.username;
-        adminProfile[newMember.user.id]['avatarURL'] = newMember.user.avatarURL;
-        fs.writeFile(config.webroot + "/adminProfile.json", JSON.stringify(adminProfile), function(err) {
-                if(err) {
-                        return console.log(err);
-                }
-        });
-});
+  (oldMember, newMember) => {
+    adminProfile[newMember.user.id] = {};
+    adminProfile[newMember.user.id]['username'] = newMember.user.username;
+    adminProfile[newMember.user.id]['avatarURL'] = newMember.user.avatarURL;
+    fs.writeFile(config.webroot + "/adminProfile.json", JSON.stringify(adminProfile), function(err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  });
 
-
+client.on('message',
+  (message) => {
+      destruct(message);
+  });
 
 client.login(token);
+
+function destruct(msg) {
+  var msg_t = msg.split(" ");
+  for (var w1 in msg) {
+    for (var w2 in db) {
+      if ( comp(w1,w2) < 10 ) {
+        Console.log(w1 + " " + w2)
+      }
+    }
+  }
+}
+
+function comp(w1, w2) {
+  var s = 0;
+  var word1 = JSON.parse(fs.readFileSync('word.json'));
+  var word2 = JSON.parse(fs.readFileSync('word.json'));
+
+  for (var l in w1) {
+    if (word1[l]) {
+      word1[l] += 1;
+    }
+  }
+
+  for (var l in w2) {
+    if (word2[l]) {
+      word2[l] += 1;
+    }
+  }
+
+  for (var l in word1) {
+    s += Math.pow((word1[l] - word2[l]), 2);
+  }
+  return s;
+}
