088af83456e02f84d8207566f4591a6c51487d4d
hugo <Lokarde>
2017-12-03 17:37:01 +0100

    correction + youtube

--- InfoscordBot.js
+++ InfoscordBot.js
@@ -1,17 +1,49 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const config = require('./config.json')
+const readline = require('readline');
+const google = require('googleapis');
+const googleAuth = require('google-auth-library');
 const client = new Discord.Client();
 
-const db = JSON.parse(fs.readFileSync(config.webroot + "/db.json"));
 const token = config.token;
+
+const db = JSON.parse(fs.readFileSync(config.webroot + "/db.json"));
+
 
 var adminProfile = JSON.parse(fs.readFileSync(config.webroot + "/adminProfile.json"));
 var buff = JSON.parse('{}');
 var time_count = JSON.parse('{}');
 var msg_bot = [];
 
+var SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+var TOKEN_DIR = config.token_dir;
+var TOKEN_PATH = TOKEN_DIR + "/token.json";
+
+fs.readFile('client_secret.json', (err, content) => {
+  if (err) {
+    console.log('Error loading client secret file: ' + err);
+    return;
+  }
+  setInverval(() => {
+    authorize(JSON.parse(content), {
+      'params': {
+        'id': 'UC_x5XG1OV2P6uZZ5FSM9Ttw',
+        'part': 'snippet,contentDetails,statistics'
+      }
+    }, channelsListById);
+  }, config.youtube_time);
+});
+
 client.on('ready', () => {
+  setInterval(() => {
+    authorize(JSON.parse(token), {
+      'params': {
+        'id': 'UC_x5XG1OV2P6uZZ5FSM9Ttw',
+        'part': 'snippet,contentDetails,statistics'
+      }
+    }, channelsListById);
+  }, config.youtube_time);
   console.log('I am ready!');
 });
 
@@ -44,12 +76,12 @@ client.on('messageReactionAdd',
     var channel_name = messageReaction.message.channel.name
     if (messageReaction.message.author.username == config.bot_name && messageReaction.emoji.name == config.emoji_correct) {
       var corrected = false
-      for (w in buff[channel_name]) {
-        while (msg_channel(channel_name, [buff[channel_name][w]], false) != channel_name) {
+      while (msg_channel(channel_name, [buff[channel_name][w]], false) != channel_name) {
+        for (w in buff[channel_name]) {
           ++db["word"][buff[channel_name][w]]["channel"][channel_name]["count"];
           ++db["channel"][channel_name]["count"];
-          corrected = true;
         }
+        corrected = true;
       }
       console.log(channel_name + " corrected:", corrected);
       messageReaction.message.delete();
@@ -135,7 +167,7 @@ function destruct(channel, msg) {
   fs.writeFileSync(config.webroot + "/db.json", JSON.stringify(db));
 }
 
-function comp(w1 = "", w2 = "") {
+function comp(w1, w2) {
   var s = 0;
   var word1 = JSON.parse(fs.readFileSync('word.json'));
   var w1_t = w1.toLowerCase().split("");
@@ -156,7 +188,7 @@ function comp(w1 = "", w2 = "") {
   return s;
 }
 
-function msg_channel(channel, msg, debug = true) {
+function msg_channel(channel, msg, debug) {
   var msg_c = 0;
   var chs = JSON.parse('{}');
   var ch_simw = [];
@@ -224,4 +256,148 @@ function msg_channel(channel, msg, debug = true) {
     return db["channel"][ch_m]["name"];
   }
   return channel;
+}
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, requestData, callback) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, requestData, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client, requestData);
+    }
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, requestData, callback) {
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client, requestData);
+    });
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
+}
+
+/**
+ * Remove parameters that do not have values.
+ *
+ * @param {Object} params A list of key-value pairs representing request
+ *                        parameters and their values.
+ * @return {Object} The params object minus parameters with no values set.
+ */
+function removeEmptyParameters(params) {
+  for (var p in params) {
+    if (!params[p] || params[p] == 'undefined') {
+      delete params[p];
+    }
+  }
+  return params;
+}
+
+/**
+ * Create a JSON object, representing an API resource, from a list of
+ * properties and their values.
+ *
+ * @param {Object} properties A list of key-value pairs representing resource
+ *                            properties and their values.
+ * @return {Object} A JSON object. The function nests properties based on
+ *                  periods (.) in property names.
+ */
+function createResource(properties) {
+  var resource = {};
+  var normalizedProps = properties;
+  for (var p in properties) {
+    var value = properties[p];
+    if (p && p.substr(-2, 2) == '[]') {
+      var adjustedName = p.replace('[]', '');
+      if (value) {
+        normalizedProps[adjustedName] = value.split(',');
+      }
+      delete normalizedProps[p];
+    }
+  }
+  for (var p in normalizedProps) {
+    // Leave properties that don't have values out of inserted resource.
+    if (normalizedProps.hasOwnProperty(p) && normalizedProps[p]) {
+      var propArray = p.split('.');
+      var ref = resource;
+      for (var pa = 0; pa < propArray.length; pa++) {
+        var key = propArray[pa];
+        if (pa == propArray.length - 1) {
+          ref[key] = normalizedProps[p];
+        } else {
+          ref = ref[key] = ref[key] || {};
+        }
+      }
+    };
+  }
+  return resource;
+}
+
+
+function channelsListById(auth, requestData) {
+  var service = google.youtube('v3');
+  var parameters = removeEmptyParameters(requestData['params']);
+  parameters['auth'] = auth;
+  service.channels.list(parameters, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    console.log(response);
+  });
 }
