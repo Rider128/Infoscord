da6e2e593785a4e15f69cd8b29f3f70455cc638e
hugo <Lokarde>
2017-12-03 11:00:19 +0100

    correction

--- InfoscordBot.js
+++ InfoscordBot.js
@@ -42,7 +42,7 @@ client.on('guildMemberUpdate',
 client.on('messageReactionAdd',
   (messageReaction, User) => {
     var channel_name = messageReaction.message.channel.name
-    if (messageReaction.message.author == "Infoscord") {
+    if (messageReaction.message.author == "Infoscord" && messageReaction.Emoji.name == "e1") {
       var corrected = false
       while (msg_channel(channel_name, buff[channel_name], false) != channel_name) {
         for (w in buff[channel_name]) {
